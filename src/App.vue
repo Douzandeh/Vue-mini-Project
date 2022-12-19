1e29204143f8af6877842342e7eb216ef2cc5f59
Hossein <hosseindozandeh799@gmail.com>
2022-12-19 20:50:09 +0330

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>|
-    <router-link to="/profile">Profile</router-link>
-
+  <nav id="nav" class="navbar navbar-expand-lg mb-5" style="background-color: #e3f2fd;">
+    <div class="container-fluid">
+      <router-link class="navbar-brand active" to="/">Navbar</router-link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor03" aria-controls="navbarColor03" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarColor03">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/">Home</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link active" to="/about">About</router-link>
+          </li>
+          <li class="nav-item">
+           <router-link class="nav-link active" to="/profile">Profile</router-link>
+          </li>
+        </ul>
+        <form class="d-flex" role="search">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-primary" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
   </nav>
-  <router-view />
+  <router-view class="container" />
 </template>
 
 <style>
@@ -17,16 +36,16 @@
   color: #2c3e50;
 }
 
-nav {
-  padding: 30px;
-}
-
-nav a {
+#nav a {
   font-weight: bold;
   color: #2c3e50;
 }
 
-nav a.router-link-exact-active {
+.navbar-brand{
+  color: #2c3e50 !important;
+}
+
+#nav a.router-link-exact-active {
   color: #42b983;
 }
 </style>
--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="Profile">
-      <h1>Profile</h1>
+  <div class="Profile">
+    <h1>Profile</h1>
+    <div class="alert alert-success">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque iure eum
+      culpa saepe nostrum pariatur, impedit, molestiae eaque, facilis nesciunt
+      deleniti blanditiis nemo? Aut id impedit, illo quibusdam asperiores
+      voluptatum.
     </div>
-  </template>
+  </div>
+</template>
